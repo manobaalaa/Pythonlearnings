461402a995b5f967d88a3c09fb2a08cc10b8365f
MANOBALA <manobaalaa@gmail.com>
2020-06-24 05:34:53 -0400

    Second commit

---
+++ Calendar.py
@@ -0,0 +1,15 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import calendar
+>>> yy=2020
+>>> mm=6
+>>> print(calendar.month(yy,mm))
+     June 2020
+Mo Tu We Th Fr Sa Su
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30
+
+>>> 
